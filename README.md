5d58270107a54bd7c02f734be14886b816fc1add
Pedro Pedro <pedro.afonso.pedro.pp@gmail.com>
2025-10-21 16:14:47 +0100

    model schema

---
+++ README.md
@@ -0,0 +1,12 @@
+# log entry
+* title - text
+* description - text
+* comment - text
+* rating  - 1-10
+* img - text (url)
+* start date - datetime
+* end date - datetime
+* lat - number
+* long - number
+* created at - datetime
+* updated at - datetime
--- server/src/index.js
+++ server/src/index.js
@@ -3,6 +3,8 @@ const morgan = require('morgan');
 const helmet = require('helmet');
 const cors = require('cors');
 
+const middlewares = require('./middlewares');
+
 const app = express();
 app.use(morgan('common'));
 app.use(helmet());
@@ -14,20 +16,8 @@ app.get('/', (req, res) => {
     res.json({ message: 'Hello, world!' });
 });
 
-app.use((req, res, next) => {
-    const error = new Error(`Not Found - ${req.originalUrl}`);
-    res.status(404);
-    next(error);
-});
-
-app.use((err, req, res, next) => {
-    const statusCode = res.statusCode === 200 ? 500 : res.statusCode;
-    res.status(statusCode);
-    res.json({
-        message: err.message,
-        stack: process.env.NODE_ENV === 'production' ? 'not production' : err.stack,
-    });
-});
+app.use(middlewares.notFound);
+app.use(middlewares.errorHandler);
 
 const port = process.env.PORT || 1337;
 
